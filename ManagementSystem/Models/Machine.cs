d7369602a639bd8fde1826aedd381c2dfd39ef05
klarahoglund <klarahoglund@hotmail.com>
2022-10-05 09:58:57 +0200

    till David

---
+++ ManagementSystem/Models/Machine.cs
@@ -0,0 +1,12 @@
+﻿namespace ManagementSystem.Models
+{
+    public class Machine
+    {
+
+
+        public string MachineName { get; set; } = string.Empty;
+        public int Id { get; set; }
+        //public bool Status { get; set; }
+
+    }
+}
---
+++ ManagementSystem/Pages/Fetchdata.razor.cs
@@ -0,0 +1,16 @@
+﻿using ManagementSystem.Models;
+using System.Net.Http.Json;
+
+namespace ManagementSystem.Pages
+{
+    public partial class Fetchdata
+    {
+        private Machine[]? forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await Http.GetFromJsonAsync<Machine[]>("sample-data/weather.json");
+            Console.WriteLine();
+        }
+    }
+}
